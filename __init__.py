834e222823e0465c6eb3f6a179a85e174bc15e1a
5axes <5axes@users.noreply.github.com>
2022-05-24 11:16:53 +0200

    V1.0.2

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2022 5@xes
-# The SlowZ is released under the terms of the AGPLv3 or higher.
+# The CuraSlowZ is released under the terms of the AGPLv3 or higher.
 
 from . import CuraSlowZ
 
