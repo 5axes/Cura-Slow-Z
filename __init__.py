3fb6f62f1c22261b7ee7f52b9682d30d186a8321
5axes <5axes@users.noreply.github.com>
2022-05-24 11:12:38 +0200

    Change to CuraSlowZ

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 5@xes
 # The SlowZ is released under the terms of the AGPLv3 or higher.
 
-from . import SlowZ
+from . import CuraSlowZ
 
 
 def getMetaData():
     return {}
 
 def register(app):
-    return {"extension": SlowZ.SlowZ()}
+    return {"extension": CuraSlowZ.CuraSlowZ()}
